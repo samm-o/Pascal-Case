6833278bbbc6a10c2a977482406519aff752a84f
samm-o <67648665+samm-o@users.noreply.github.com>
2023-01-30 11:01:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pascal_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type words seperated by a space: ");
+            var input = Console.ReadLine();
+
+            var words = input.Split(" ");
+            for (var i = 0; i < words.Length; i++) 
+                words[i] = char.ToUpper(words[i][0]).ToString() + words[i].Substring(1).ToLower();
+            Console.WriteLine(string.Join("", words));
+        }
+    }
+}
